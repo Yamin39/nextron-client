ae834648434e2592ba5676fa27c2b807eb05b594
mdmufazzalhossa <33011930-mdmufazzalhossa@users.noreply.replit.com>
2024-08-15 13:05:07 +0000

    added register page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Products from "../pages/Products/Products";
 import Login from "../pages/Login/Login";
-
+import Register from "../pages/Register/Register";
 import Root from "../layouts/Root";
 
 const router = createBrowserRouter([
@@ -18,6 +18,11 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      // register route
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
