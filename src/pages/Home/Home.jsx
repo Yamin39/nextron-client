c934b91daa80046d926e392a2124a561b3d9b82d
mdmufazzalhossa <33011930-mdmufazzalhossa@users.noreply.replit.com>
2024-08-15 09:38:33 +0000

    project initialized

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center">Welcome to Nextron</h1>
+      <p className="text-lg text-center">
+        This is a simple React application that uses Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
